8e19d14552c37ea45369b670a708e6c34b18f975
Andrea Fomera <afomera@hey.com>
2022-02-20 10:25:45 -0700

    generate focus areas after the user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :accomplishments, dependent: :destroy
+  has_many :focus_areas, dependent: :destroy
+
+  after_create do
+    focus_areas.create(name: "Personal", color: "#7C3AED")
+    focus_areas.create(name: "Work", color: "#E11D48")
+    focus_areas.create(name: "Health", color: "#2563EB")
+  end
 end
